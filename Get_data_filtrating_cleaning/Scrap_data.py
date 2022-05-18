7c1bcc9e3717bd702585e2dd63c6f02455273758
JacekKarnowka <jacek.karnowka@gmail.com>
2022-05-18 09:13:41 +0200

    Formating files, split styles to separate file Styles.py

--- Get_data_filtrating_cleaning/Scrap_data.py
+++ Get_data_filtrating_cleaning/Scrap_data.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import  pandas as pd
+import pandas as pd
+
 pd.set_option("display.max_rows", 50, "display.max_columns", 10)
 
-df_links = pd.DataFrame(columns= ['Link'])
+df_links = pd.DataFrame(columns=["Link"])
 
 base_link = "https://hubblehq.com/office-space-london?peopleMax=25&peopleMin=10&page=1"
 r = requests.get(base_link, "html.parser")
-soup = bs(r.content, 'html.parser')
+soup = bs(r.content, "html.parser")
 
 # find html responsible for page chooser
 # find number of max pages
-max_number_info = soup.find(class_ = "css-zk8w31-paginationStyle")
-max_pages_number = int(str(max_number_info.find_all('li')).split('<!-- -->')[2][:2])
+max_number_info = soup.find(class_="css-zk8w31-paginationStyle")
+max_pages_number = int(str(max_number_info.find_all("li")).split("<!-- -->")[2][:2])
 
 base_path = "https://hubblehq.com/office-space-london?peopleMax=25&peopleMin=10&"
 
@@ -22,46 +23,55 @@ for i in range(1, max_pages_number):
     page = "page={}".format(i)
     current_page = base_path + page
 
-    print('page number: {}, link: {}'.format(i, current_page))
+    print("page number: {}, link: {}".format(i, current_page))
 
     r = requests.get(current_page, "html.parser")
     soup = bs(r.content, "html.parser")
 
     # look for specific html class containing links
-    page_links_info = soup.findAll(class_ = 'css-6rnjt4-wrapperStyle')
+    page_links_info = soup.findAll(class_="css-6rnjt4-wrapperStyle")
 
     # get all links from page_links_info
     for link in page_links_info:
-        all_links.append(link.find(class_ = 'css-v7ho7-linkStyle', href = True).get('href'))
+        all_links.append(link.find(class_="css-v7ho7-linkStyle", href=True).get("href"))
 
 # print(all_links)
 
 df_main = pd.DataFrame()
 
 for link in all_links:
-    first = 'https://hubblehq.com'
+    first = "https://hubblehq.com"
     good_link = first + link
     print(good_link)
     r = requests.get(good_link, "html.parser")
     soup = bs(r.content, "html.parser")
 
-    map_link = soup.findAll('img', attrs={'srcset': True})
-    coordinates = map_link[-1]['srcset'].split('center=')[1].split('&zoom')[0]
-    Lat = coordinates.split(',')[0]
-    Long = coordinates.split(',')[1]
+    map_link = soup.findAll("img", attrs={"srcset": True})
+    coordinates = map_link[-1]["srcset"].split("center=")[1].split("&zoom")[0]
+    Lat = coordinates.split(",")[0]
+    Long = coordinates.split(",")[1]
 
-    details = soup.findAll(class_ = "css-5i54kf-officeDetailStyle")
+    details = soup.findAll(class_="css-5i54kf-officeDetailStyle")
 
     lst_dict = []
 
     for i in range(0, len(details)):
 
-        office_price = details[i].find(
-            class_="css-ubngax-wrapperStyle-marginlessStyle-contentText1-baseOverridesStyle").get_text()
+        office_price = (
+            details[i]
+            .find(
+                class_="css-ubngax-wrapperStyle-marginlessStyle-contentText1-baseOverridesStyle"
+            )
+            .get_text()
+        )
 
-        local_information = details[i].findAll(class_="css-jqw1nl-textStyle-contentText2")
+        local_information = details[i].findAll(
+            class_="css-jqw1nl-textStyle-contentText2"
+        )
 
-        title = soup.find(class_="css-1ky4310-headingStyle-titleText1-colorStyle").get_text()
+        title = soup.find(
+            class_="css-1ky4310-headingStyle-titleText1-colorStyle"
+        ).get_text()
 
         if len(local_information) == 3:
 
@@ -86,11 +96,19 @@ for link in all_links:
             min_term = "No information"
             building_floor = "No information"
 
-        lst_dict.append({'Link': good_link, 'Title': title,
-                         'Option': i, 'Office price': office_price,
-                         'People': people, 'Min. Term': min_term,
-                         'Building Floor': building_floor, 'Latitude' : Lat,
-                         'Longitude': Long})
+        lst_dict.append(
+            {
+                "Link": good_link,
+                "Title": title,
+                "Option": i,
+                "Office price": office_price,
+                "People": people,
+                "Min. Term": min_term,
+                "Building Floor": building_floor,
+                "Latitude": Lat,
+                "Longitude": Long,
+            }
+        )
 
         df_dict = pd.DataFrame(lst_dict)
 
@@ -98,8 +116,4 @@ for link in all_links:
 
 print(df_main.head(10))
 
-df_main.to_excel('All_data_no_filtrated.xlsx', engine='xlsxwriter')
-
-
-
-
+df_main.to_excel("All_data_no_filtrated.xlsx", engine="xlsxwriter")
---
+++ Styles.py
@@ -0,0 +1,44 @@
+GRAPH_LINES_COLOR = "#636363"
+
+PLOT_BGCOLOR = "#1e1e1e"
+
+LEFT_MENU_COLOR = "#2b2b2b"
+
+TEXT_STYLE_LEFT = {
+    "font-size": 20,
+    "font-family": "sans-serif",
+    "color": "white",
+    "verticalAlign": "top",
+    "marginTop": "20%",
+    "marginLeft": "5%",
+    "marginRight": "5%",
+}
+
+TEXT_STYLE_RIGHT = {
+    "font-size": 20,
+    "font-family": "sans-serif",
+    "color": "white",
+    "verticalAlign": "top",
+    "marginLeft": "5%",
+}
+
+RIGHT_MENU_COLOR = "#484848"
+
+TEXT_STYLE = {
+    "fontSize": 18,
+    "paddingRight": "30px",
+}
+
+GRAPH_DIV_STYLE = {
+    "display": "inline-block",
+    "verticalAlign": "top",
+    "horizontalAlign": "right",
+    "width": "80%",
+    "height": "1500px",
+    "color": "#657895",
+    "background-color": RIGHT_MENU_COLOR,
+}
+
+Graph_Height = 650
+Graph_Width = 850
+SLIDER_WIDTH = Graph_Width - 100
